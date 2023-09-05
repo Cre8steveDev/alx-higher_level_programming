4f91122b6eddfece9ee25578239879153288cfca
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-06 00:19:22 +0100

    Optimizing Linked List to add a node in an ordered list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -9,10 +9,31 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
+	listint_t *current = *head;
+	listint_t *new_node = NULL;
+	listint_t *temp = NULL;
+
 	(void)number;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Traverse the list to find where current->n > number*/
+	while (current->n < number && current != NULL)
+	{
+		if (current->next->n > number)
+			break;
+		current = current->next;
+	}
+	new_node->n = number;
+	temp = current->next;
+	current->next = new_node;
+	new_node = temp;
 
 	return (NULL);
 }
