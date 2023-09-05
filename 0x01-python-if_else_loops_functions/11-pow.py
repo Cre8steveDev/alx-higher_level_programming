3a3b72dc9dc58020df211cdd06d5c541dd1f2df0
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-05 22:45:52 +0100

    Trying the gods

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -4,20 +4,6 @@ def pow(a, b):
     if b == 0:
         return 1
 
-    val = 0
+    b = abs(b)
 
-    a = a if a > 0 else abs(a)
-
-    if b > 0:
-        while b != 1:
-            val += a ** a
-            b -= 1
-    elif b < 0:
-        # Very bad coding. Will find an improvement
-        if a == 2 and b == -2:
-            return 4
-        if b == -2 and a == 10:
-            return 0.01
-        elif a == -98 and b == -10:
-            return float(1.223881142011411e-20)
-    return val
+    return a ** b
