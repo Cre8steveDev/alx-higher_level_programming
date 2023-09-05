5e9a98f716d35567509f7146c36dfb24b4df6492
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-05 23:25:00 +0100

    Inserting a node in a linked List Task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * insert_node - Inserts node
+ * @head: Head of the linked list
+ * @number: number to be added to the list
+ * Return: Pointer to a node or null if it fails
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	if (head == NULL || *head == NULL)
+		return (NULL);
+}
