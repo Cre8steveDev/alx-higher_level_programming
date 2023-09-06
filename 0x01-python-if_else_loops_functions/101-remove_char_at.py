24d7154570c193641c3e2740fba8a6f3ccdedf1e
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-06 01:41:39 +0100

    Remove character at index

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def remove_char_at(str, n):
+    newStr = ""
+    for num in range(len(len(str))):
+        if (num == n):
+            continue
+        newStr += str[num]
+    return newStr
