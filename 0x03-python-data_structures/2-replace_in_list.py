c44bb7691e1ca63b0cfa037f4e0fceec97f3ee43
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-09 11:44:00 +0100

    Reverse list with reverse function 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 def replace_in_list(my_list, idx, element):
     len_t = len(my_list)
 
-    if idx < 0 or idx > len_t:
+    if idx < 0 or idx >= len_t:
         return my_list
 
     my_list[idx] = element
