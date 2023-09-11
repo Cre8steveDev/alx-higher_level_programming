c8274b5ddc79c58aad766a8b5cc5cc9cac671fc7
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-11 11:07:09 +0100

    Palindrome problem

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,8 +11,8 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *ptr;
-	int array[1000] = {0};
-	int len = 0, r_idx = 999;
+	int array[1500] = {0};
+	int len = 0, r_idx = 1499;
 
 	if (head == NULL)
 		return (1);
