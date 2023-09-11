00509450c9d1aeb5d6d54c0fb5964f083d6d72a4
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-11 11:13:17 +0100

    Palindrome problem

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -12,7 +12,7 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *ptr;
 	int array[3000] = {0};
-	int len = 0, r_idx = 2999;
+	int len = 0, r_idx = 2999, tac = 0;
 
 	if (head == NULL)
 		return (1);
@@ -31,13 +31,15 @@ int is_palindrome(listint_t **head)
 	}
 	ptr = *head;
 	r_idx++;
+	tac = len / 2;
 
-	while (ptr != NULL)
+	while (ptr != NULL && tac > 0)
 	{
 		if (ptr->n != array[r_idx])
 			return (0);
 		r_idx++;
 		ptr = ptr->next;
+		tac--;
 	}
 
 	return (1);
