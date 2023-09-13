0b58aa4db707db7f9b0cde8bad53f7dd4f41818c
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-13 21:09:44 +0100

    Unique Add 1

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    uniqu = []
-
-    for itr in my_list:
-        if itr in uniqu:
-            continue
-        uniqu.append(itr)
-
-    return sum(uniqu)
+    return sum(set(my_list))
