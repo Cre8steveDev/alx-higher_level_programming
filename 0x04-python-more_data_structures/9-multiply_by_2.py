466ae3cce11a4af24b4d71da436b28ae5a215d2d
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-13 22:35:24 +0100

    Update dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,7 @@ def multiply_by_2(a_dictionary):
 
     new_dictionary = a_dictionary.copy()
 
-    for key in new_dictionary:
-        new_dictionary[key] *= 2
+    for key, values in new_dictionary.items():
+        new_dictionary[key] = values * 2
 
     return new_dictionary
