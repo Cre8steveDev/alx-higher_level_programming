b2c592ce4ec05e960bd30fb150e663c08a90d684
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-26 21:41:08 +0100

    Setting a class with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A Square class that will be used to instantiate an object"""
+
+
+class Square:
+
+    """An ampty class Square that defines a square.
+    It currently has no attributes nor properties.
+    """
+
+    def __init__(self, size):
+        """Initialization method of the class
+
+                Args:
+                        size (int): Size of the square
+        """
+        self.__size = size
