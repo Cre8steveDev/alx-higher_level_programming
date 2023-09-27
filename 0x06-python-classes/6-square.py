f4d6ca3a25319cdcf6810ee60504a46a378c08aa
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-09-27 21:44:22 +0100

    Square with printing funny

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,7 +39,8 @@ class Square:
     @position.setter
     def position(self, value):
         """Setter method for the position attribute"""
-        if not isinstance(value, tuple) or len(value) != 2 or value[0] < 0 or value[1] < 0:
+        if not isinstance(value, tuple) or len(value) != 2\
+                or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
