7e8814f8f8ee6487f281ae830c53a9f24ccef2d8
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-10-09 20:57:38 +0100

    Another one

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,3 +16,8 @@ class BaseGeometry():
             raise TypeError("<name> must be an integer")
         if value <= 0:
             raise ValueError("<name> must be greater than 0")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
