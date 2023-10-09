f9d104f710e100caeb19902c91c292c2806571fd
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-10-09 19:54:40 +0100

    Test case passed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,12 +4,14 @@
 
 
 class MyList(list):
+
     """MyList inherits from the list class"""
 
     def print_sorted(self):
         """Sorts the elements in the class"""
         list_sorted = sorted(self)
         print(list_sorted)
+
 
 if __name__ == "__main__":
     import doctest
