3d532b5f94ccff1556957e59a6ad4a6f09c84880
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-10-09 22:01:36 +0100

    Addva

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class MyInt(int):
+    """A New Int that rebels"""
+
+    def __eq__(self, other):
+        # Override == (equality) to behave as !=
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        # Override != (inequality) to behave as ==
+        return super().__eq__(other)
