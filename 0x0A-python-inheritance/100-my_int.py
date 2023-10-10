fb7bfc5d827348faaf7efb9f067a0cb609656927
Cr8steveDesign <cr8stevestudios@gmail.com>
2023-10-10 01:48:12 +0100

    More

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,10 +6,6 @@
 class MyInt(int):
     """A New Int that rebels"""
 
-    def __init__(self, value):
-        """Instantiate class"""
-        super().__init__(value)
-
     def __eq__(self, other):
         # Override == (equality) to behave as !=
         return super().__ne__(other)
