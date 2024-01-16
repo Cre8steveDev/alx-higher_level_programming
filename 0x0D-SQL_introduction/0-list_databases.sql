b592716646033c415095363a3a5b164cd085e0c8
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-16 22:28:25 +0100

    Introduction to SQL - Tasks

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,4 @@
+-- List all databases in the server
+-- Introduction to SQL | MySQL
+
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Introduction to SQL | MySQL
+-- Create a Database if it doesn't exist
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
