8056e111a3b0dbb595dbf098dc0fd433b53f49c4
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-16 23:45:54 +0100

    Some More Additions

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Sorting a result from table in descending order
+-- Using the ORDER BY a particular field or fields
+-- You can pass multiple ordering values
+
+SELECT score, `name` FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,8 @@
 -- Adding Information into a Table in the currently selected database
 -- Using INSERT INTO - for this example, we're assuming that the name of 
 -- the database is already given to the command launch 
+-- For case of multiple values, they should be in individual brackets
+-- separated by comma e.g
+-- VALUES (1, "Steve"), (2, "Thomas"), ...;
 
 INSERT INTO first_table (id, name) VALUES (89, 'Best School');
