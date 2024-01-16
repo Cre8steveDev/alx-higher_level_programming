33b5402cfada39c30904e9899ea8371ce23594cc
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-16 22:48:00 +0100

    Showing tables

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Introduction to SQL | MySQL
+-- Deleting a Database
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,6 @@
+-- Introduction to SQL | MySQL
+-- Show all tables in a database selected during launch
+-- e.g echo "SHOW TABLES;" | sudo mysql <database_name>
+-- or echo "SHOW TABLES;" | mysql -u <user> -h <hostname> -p <database_name>
+
+SHOW TABLES;
