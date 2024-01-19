58dc64e46e052ee64dc0f9a6e33829c5b7a6797d
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-19 22:07:55 +0100

    Defining foreign and primary keys 2

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,11 +7,14 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table cities already exists, your script should not fail
 
+-- Using AUTO_INCREMENT automatically makes that field unique
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
-	name VARCHAR(256 NOT NULL)
-	PRIMARY KEY (id)
+	name VARCHAR(256 NOT NULL),
+	PRIMARY KEY (id),
 	FOREIGN KEY (state_id) REFERENCES states(id)
 );
