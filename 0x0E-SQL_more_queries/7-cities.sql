254d9f72aea184ee0a81a4dd23122fefbac4b367
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-19 22:13:28 +0100

    Defining foreign and primary keys 4

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -11,10 +11,10 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256 NOT NULL),
 	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES states(id)
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usastates(id)
 );
