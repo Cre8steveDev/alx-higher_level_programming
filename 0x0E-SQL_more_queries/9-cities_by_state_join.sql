31256c725189d3a3e8a8907855d7b32dafc11f89
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-01-20 15:10:54 +0100

    joins 2

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,7 @@
+-- Using Inner Join to connect two tables that match on certain
+-- Values, in this case join parts of the tables where the current
+-- cities's value state_id is equal to the states table value.id
+
 SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id = states.id
