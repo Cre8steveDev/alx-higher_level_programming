af8373ea75d89f68cc1bec8b9cc9c038ac9808db
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-12 23:22:46 +0100

    Weird

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 
 const args = process.argv;
-let count = 0;
 
-function countArgs (args) {
-  for (const i in args) {
-    count = i + 1;
-  }
-}
-
-countArgs(args);
-
-if (count < 3) {
+if (!args[2]) {
   console.log('No argument');
 } else {
   console.log(args[2]);
