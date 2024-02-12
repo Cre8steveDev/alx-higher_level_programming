0b97444db8675c4aa6a1dd5778820185e043df74
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-12 23:52:55 +0100

    Parsing an integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+
+if (parseInt(args, 10).toString() === args){
+  console.log(`My number: ${parseInt(args, 10)}`)
+}else {
+  console.log('Not a number');
+}
