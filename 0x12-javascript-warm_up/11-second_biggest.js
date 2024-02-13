604b198d21bf1200b80dedb3c99a6192926cfc76
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-13 17:38:17 +0100

    Second Biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Get argument from command line
+const args = process.argv;
+
+// Print 0 if there's no argument or if just one argument in list
+if (args.length < 4) {
+  console.log(0);
+} else {
+  const sortedArr = args.toSorted();
+  console.log(sortedArr[3]);
+}
