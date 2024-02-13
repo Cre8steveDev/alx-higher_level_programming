2f4256096280338d64f2a8cebe772d9ac7440be9
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-13 17:56:43 +0100

    Update an attribute of an object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// Code from the intranet; update value to become 89
+//
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.value = 89;
+
+console.log(myObject);
