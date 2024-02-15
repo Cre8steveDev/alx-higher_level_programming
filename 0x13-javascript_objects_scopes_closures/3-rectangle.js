6dafe681e034a4b0ba9ad38807f79705bab6f78d
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-15 08:32:07 +0100

    First Class Method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+// Write a class Rectangle that defines a rectangle with other details
+
+class Rectangle {
+  // Constructor function of the class
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // Create an instance method caled print that prints the rect using X
+	print () {
+		for (const i = 0; i < this.height; i++){
+			for (const j = 0; j < this.width; j++){
+				console.log('X');
+			}
+		}
+	}
+}
+
+module.exports = Rectangle;
