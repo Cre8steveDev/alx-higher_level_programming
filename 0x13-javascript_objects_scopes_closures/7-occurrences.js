2be2b0f998123cf95155159146e038c6fdced6bb
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-15 10:08:09 +0100

    7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 // Write a function that returns the number of occurences in a list
+// Export
 
 exports.nbOccurences = function (list, searchElement){
    let newArr = list.filter(item => item === searchElement);
