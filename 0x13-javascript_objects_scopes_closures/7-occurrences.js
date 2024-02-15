c1c0909808bac9173bbbbc13264ccace1f823dda
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-15 10:10:09 +0100

    Semistandard

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 // Write a function that returns the number of occurences in a list
 // Export
 
-exports.nbOccurences = function (list, searchElement){
-   let newArr = list.filter(item => item === searchElement);
-   return newArr.length;
-}
+exports.nbOccurences = function (list, searchElement) {
+  const newArr = list.filter(item => item === searchElement);
+  return newArr.length;
+};
