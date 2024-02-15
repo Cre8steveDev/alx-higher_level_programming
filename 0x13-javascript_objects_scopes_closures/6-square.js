bb67ffd1b97f7a0cf45b948bd422539240bde0cb
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-15 09:36:43 +0100

    Constructor

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Write a class Square that defines a square and inherits from Square of
+// 5-square.js
+
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
