e12027257db4a2a7c6f22a70c88ae383fbb23cc4
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-18 18:28:18 +0100

    Basic Validation to prevent SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""Write a script that lists all states with a name starting
+with N (Uppercase N) from the database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    # Get the Command line arguments
+    _, username, password, dbName, searchName = sys.argv
+    searchName = str(searchName)
+
+    # Preventing possible SQL Injection using text validation
+    # Check to ensure the user input is an alphanumeric character,
+    # and it doesn't contain any special character
+
+    if not searchName.isalnum():
+        sys.exit()
+
+    # Make connection to the database with MySQLdb module
+    db = MySQLdb.connect(host='localhost', user=username,
+                         passwd=password, db=dbName)
+
+    # Get the cursor object from the db
+    cur = db.cursor()
+
+    # Execute your query on the cursor object
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY'{}' ORDER BY states.id ASC"
+                .format(searchName))
+
+    results = cur.fetchall()
+
+    # Print the data returned
+    for tup in results:
+        print(tup)
