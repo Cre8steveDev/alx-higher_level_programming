dd833a2c919e4b56f3a3ed1e3c1bd642f4106ae8
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-02-18 16:58:18 +0100

    Task 0 - Using MySQLdb python module to write sql code inside python

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa"""
+# import the sys module to access command line
+# and the MySQLdb module
+import sys
+import MySQLdb
+
+# Retrieve arguments from the command line
+_, username, password, dbName = sys.argv
+
+# Connect to the MySQL Server if on local host or to remote machine
+db = MySQLdb.connect(host='localhost', user=username, passwd=password, db=dbName)
+# Retrieve cursor from the db instance
+cur = db.cursor()
+
+# Execute a MySQL Command on the cursor object
+cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+# Use the fetchall method on the cursor to retrieve the results
+result = cur.fetchall()
+
+# Print result to see output
+for tup in list(result):
+    print(tup)
---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Write a script that imports a dictionary of ocurences by user id and
+// computes a dictionary of uder ids by occurence
+
+const dict = require('./101-data').dict;
+
+
