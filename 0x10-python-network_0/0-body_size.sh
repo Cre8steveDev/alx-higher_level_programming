13ea5c53f9b883ec87a803b3351a062507307698
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-01 08:12:36 +0100

    Make it just 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Making a Request to a server using Curl and printing 
-# just the size of the body in the Content-Length
-
-GIVEN_URL=$1
-curl -s -I "$GIVEN_URL" | grep "Content-Length" | cut -d" " -f2
+curl -s -I "$1" | grep "Content-Length" | cut -d" " -f2
