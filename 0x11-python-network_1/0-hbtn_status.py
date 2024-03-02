b2c906b6206afbbfb8f9f93e6b9954eff1063346
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-02 17:30:55 +0100

    Still on still

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
 
-import urllib.request
+from urllib.request import (Request, urlopen)
 
-# Prepare the Request object
-req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+if __name__ == "__main__":
+    # Prepare the Request object
+    req = Request("https://alx-intranet.hbtn.io/status")
 
-# Pass in the request object to the urlopen within a context
-with urllib.request.urlopen(req) as res:
-    response = res.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(response)))
-    print("\t- content: {}".format(response))
-    print("\t- utf8: {}".format(response.decode("utf-8")))
+    # Pass in the request object to the urlopen within a context
+    with urlopen(req) as res:
+        response = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8: {}".format(response.decode("utf-8")))
