9a56db20b18053fda036edda3aac57a0c99ed711
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-02 18:29:20 +0100

    Encoding data in the url of a post request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a Python script that takes in a URL and an email
+sends a POST request to the passed URL with the email as a parameter"""
+
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+from sys import argv
+
+# Get url and email from Command line
+url = argv[1]
+data = urlencode({"email": argv[2]}).encode("ascii")
+
+# create Request object
+req = Request(url, data)
+
+with urlopen(req) as response:
+    print(response.read().decode("utf-8"))
+
+
