2c6b058c1b6a9a0b719508c4f01532d0a1999dc8
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-04 10:11:42 +0100

    Sending post messages

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A python script that takes in a URL and an email
+address, sends a POST request to the passed URL"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    email_address = argv[2]
+    url = argv[1]
+
+    payload = {'email': email_address}
+    res = requests.post(url, json=payload)
+
+    # Print the body of the response
+    print(res.text)
