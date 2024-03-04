ea98e02e34623f9cc171a518b76686c38fb3f845
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-04 09:50:09 +0100

    add name checking for module import not run script

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Python Script """
-
-from sys import argv
-import requests
 """Write a Python Script that takes in a URL, sends a request to the URL and displays the """
 
-# Make request to the url
-res = requests.get(argv[1])
-req_id = res.headers["X-Request-Id"]
-print(req_id)
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    # Make request to the url
+    res = requests.get(argv[1])
+    req_id = res.headers["X-Request-Id"]
+    print(req_id)
