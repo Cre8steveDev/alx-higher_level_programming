cc368b98c5c502dd0af083f6d2e26ce59c7eb740
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-04 12:24:53 +0100

    Trying again 2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a Python script that takes 2 arguments in order to query \
+"""Write a Python script that takes 2 arguments in order to query
 the GitHub API and listst 10 commits from recent to oldest"""
 
 if __name__ == "__main__":
@@ -11,18 +11,15 @@ if __name__ == "__main__":
     header = {"accept": "application/vnd.github+json"}
     query_param = {"per_page": 10, "page": 1}
 
-    github_url = "https://api.github.com/repos/{}/{}/commits".format(owner_name, repo_name,)
+    github_url = ("https://api.github.com/repos/{}/{}/"
+                  "commits").format(owner_name, repo_name,)
 
-    res = requests.get(github_url)
+    res = requests.get(github_url, params=query_param, headers=header)
     data = res.json()
     try:
-        for i in range(10):
+        for i in range(len(data)):
             print("{}: {}".format(
                 data[i].get("sha"),
                 data[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
-
-
-
-
