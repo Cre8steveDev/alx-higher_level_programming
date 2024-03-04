bcc657953f4428796cac9bc2b397eec9f359fe0a
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-04 11:07:16 +0100

    receiving response and decode json response 2

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(body.id, body.name))
-    except JSONDecodeError as e:
+    except ValueError as e:
         print("Not a valid JSON")
