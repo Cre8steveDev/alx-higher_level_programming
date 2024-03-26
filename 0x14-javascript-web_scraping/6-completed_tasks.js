61d7b07ac0c513a8d9d130403f0a8e6899a2755a
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-26 22:40:26 +0100

    Accumulating count of completed tasks per userId

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+// Write a script that computes the number of tasks
+// Completed by user id.
+// The first argument is the API URL:
+// https://jsonplaceholder.typicode.com/todos
+// Only print users with completed task
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  const data = JSON.parse(body);
+  const todoObject = {};
+
+  data.map((datam) => {
+    if (!todoObject[datam.userId]) {
+      todoObject[datam.userId] = 0;
+    }
+
+    return (todoObject[datam.userId] += datam.completed === true ? 1 : 0);
+  });
+
+  console.log(todoObject);
+});
