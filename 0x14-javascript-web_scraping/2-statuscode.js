6af856dbb421803344d807fa0c89aeb763887770
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-03-26 21:33:35 +0100

    First request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Write a script that displays the status code of a GET request
+// The first argument is the URL to request (GET)
+// The status code must be printed like this: code: <status code>
+
+const request = require('request');
+
+request.get(process.argv[2], (err, response) => {
+  if (err) {
+    return;
+  }
+  console.log(`code: ${response.statusCode}`);
+});
